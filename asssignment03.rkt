b5a6828c5d5f679cbcf830da31ba6ea960e65ccf
Brian Fung <bfung11@gmail.com>
2016-01-31 18:50:20 -0800

    renamed var to idC moved parsing for symbol under booleans to stay consistent with assignment

--- asssignment03.rkt
+++ asssignment03.rkt
@@ -15,7 +15,7 @@
   [binop (op : symbol) 
          (l : OWQQ3) 
          (r : OWQQ3)]
-  [var (id : symbol)]
+  [idC (id : symbol)]
   [app (fn : symbol) 
        (args : (listof OWQQ3))])
 
@@ -34,6 +34,7 @@
               (values '* *)
               (values '/ /))))
 
+(define env (hash (list)))
 
 ; consumes an expression and parses and interprets it
 ; taken from Assignment 2 by John Clements
@@ -79,6 +80,7 @@
       [(s-exp-number? s) (num (s-exp->number s))]
       [(s-exp-match? `true s) (boolC #t)]
       [(s-exp-match? `false s) (boolC #f)]
+      [(s-exp-match? `SYMBOL s) (idC (s-exp->symbol s))]
       [(s-exp-match? '{ifleq0 ANY ANY ANY} s)
          (local [(define a-list (s-exp->list s))]
           (ifleq (parse (second a-list)) 
@@ -90,20 +92,19 @@
           (cond [(some? (hash-ref binop-table first-sym))
                  (binop (s-exp->symbol (first a-list)) 
                   (parse (second a-list)) (parse (third a-list)))]
-                [else (app first-sym (map parse (rest a-list)))]))]
-      [(s-exp-match? `SYMBOL s) (var (s-exp->symbol s))]))
+                [else (app first-sym (map parse (rest a-list)))]))]))
 
 (test (parse '3) (num 3))
 (test (parse `true) (boolC #t))
 (test (parse `false) (boolC #f))
 (test (parse '{ifleq0 2 0 5}) (ifleq (num 2) (num 0) (num 5)))
-(test (parse '{ifleq0 x 0 5}) (ifleq (var 'x) (num 0) (num 5)))
+(test (parse '{ifleq0 x 0 5}) (ifleq (idC 'x) (num 0) (num 5)))
 (test (parse '{+ 3 3}) (binop '+ (num 3) (num 3)))
 (test (parse '{- 3 3}) (binop '- (num 3) (num 3)))
 (test (parse '{* 3 3}) (binop '* (num 3) (num 3)))
 (test (parse '{/ 3 3}) (binop '/ (num 3) (num 3)))
-(test (parse '{/ x 3}) (binop '/ (var 'x) (num 3)))
-(test (parse `x) (var 'x))
+(test (parse '{/ x 3}) (binop '/ (idC 'x) (num 3)))
+(test (parse `x) (idC 'x))
 
 ; Parses a function definition by consuming an s-expression and produces a
 ; FundefC
@@ -120,11 +121,11 @@
 (test (parse-fundef '{func {none} {+ 4 5}}) 
       (fundef 'none (list ) (binop '+ (num 4) (num 5))))
 (test (parse-fundef '{func {pumpkin x} {+ 4 x}}) 
-      (fundef 'pumpkin (list 'x) (binop '+ (num 4) (var 'x))))
+      (fundef 'pumpkin (list 'x) (binop '+ (num 4) (idC 'x))))
 (test (parse-fundef '{func {lots x y} {+ 4 x}}) 
-      (fundef 'lots (list 'x 'y) (binop '+ (num 4) (var 'x))))
+      (fundef 'lots (list 'x 'y) (binop '+ (num 4) (idC 'x))))
 (test (parse-fundef (first (s-exp->list '{{func {f x y} {+ x y}}})))
-      (fundef 'f (list 'x 'y) (binop '+ (var 'x) (var 'y))))
+      (fundef 'f (list 'x 'y) (binop '+ (idC 'x) (idC 'y))))
 
 ; Parses a list of function definitions by consuming an s-expression and 
 ; produces a list of FundefC
@@ -134,7 +135,7 @@
 
 (test (parse-prog '{{func {f x y} {+ x y}}
                     {func {main} {f 1 2}}})
-      (list (fundef 'f (list 'x 'y) (binop '+ (var 'x) (var 'y)))
+      (list (fundef 'f (list 'x 'y) (binop '+ (idC 'x) (idC 'y)))
             (fundef 'main (list ) (app 'f (list (num 1) (num 2))))))
 (test (parse-prog '{{func {f} 5}
                     {func {main} {+ {f} {f}}}})
@@ -176,7 +177,7 @@
             [else (interp f funs)])]
     [binop (s l r) ((some-v (hash-ref binop-table s)) 
                     (interp l funs) (interp r funs))]
-    [var (id) (error 'interp "unbound identifier")]
+    [idC (id) (error 'interp "unbound identifier")]
     [app (fn args)
       (type-case FundefC (lookup fn funs)
         [fundef (name params body) (interp body funs)])]
@@ -188,7 +189,7 @@
 (test (interp (binop '- (num 3) (num 3)) (list )) 0)
 (test (interp (binop '* (num 3) (num 3)) (list )) 9)
 (test (interp (binop '/ (num 3) (num 3)) (list )) 1)
-(test/exn (interp (var 'x) (list )) "unbound identifier")
+(test/exn (interp (idC 'x) (list )) "unbound identifier")
 (test (interp (app 'f (list (num 3) (num 4))) 
               (list (fundef 'f (list 'x 'y) (binop '+ (num 2) (num 3)))))
       5)
@@ -199,9 +200,9 @@
     (type-case FundefC (lookup 'main funs)
       (fundef (name args body) (interp body funs))))
 
-; testcase - no vars
-; testcase - vars but not used
-; testcase - use vars
+; testcase - no idCs
+; testcase - idCs but not used
+; testcase - use idCs
 (test (interp-fns (list (fundef 'add (list ) (binop '+ (num 2) (num 3)))
                         (fundef 'main (list ) (app 'add (list )))))
       5)
@@ -210,9 +211,9 @@
       5)
 
 ; wrong code - required to save submission
-; (test (interp-fns (list (fundef 'f (list 'x 'y) (binop '+ (var 'x) (var 'y)))
+; (test (interp-fns (list (fundef 'f (list 'x 'y) (binop '+ (idC 'x) (idC 'y)))
 ;                         (fundef 'main (list ) (app 'f (list (num 1) (num 2))))))
 ;       3)
-; replaces all occurrences of a var with a number
+; replaces all occurrences of a idC with a number
 ; (define (subst [s : s-expression]) : s-expression
 ;   )
