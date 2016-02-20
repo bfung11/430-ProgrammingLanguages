52dde58560d826ad9df24cc00bcda5550d3b2915
Brian <bfung11@gmail.com>
2016-02-19 17:58:41 -0800

    Adding OWQQ and Value back in

--- asssignment05.rkt
+++ asssignment05.rkt
@@ -5,3 +5,25 @@
 (require plai-typed/s-exp-match)
 
 (print-only-errors #t)
+
+(define-type OWQQ3
+  [numC (n : number)]
+  [boolC (b : boolean)]
+  [idC (id : symbol)]
+  [ifC (condition : OWQQ3) 
+       (if-true : OWQQ3) 
+       (else-statement : OWQQ3)]
+  [lamC (params : (listof symbol))
+        (body : OWQQ3)]
+  [binopC (op : symbol) ; operator
+          (l : OWQQ3) 
+          (r : OWQQ3)]
+  [appC (fn : OWQQ3) 
+        (args : (listof OWQQ3))])
+
+(define-type Value
+  [numV (num : number)]
+  [boolV (bool : boolean)]
+  [cloV (params : (listof symbol))
+        (body : OWQQ3)
+        (env : Environment)])
